19ac4d5067938d7eab6708b11a9137a93f6f9162
McBalbino <cecyliasb@gmail.com>
2022-02-16 16:04:01 -0300

    iniciando projeto

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -1,18 +1,4 @@
 
 
-cadastrarComoCliente:: String -> String -> String -> Animal -> IO Cliente
-cadastrarComoCliente = do
-    putStrLn "\nInsira seu nome:"
-    nome <- getLine
-    putStrLn "\nInsira seu email:"
-    email <- getLine
-    putStrLn "\nInsira sua senha:"
-    senha <- getLine
-    putStrLn "\nInsira seu animais:"
-    animais <- getLine
-
-
-
-
 
     
