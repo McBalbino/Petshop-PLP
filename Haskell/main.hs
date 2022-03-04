1aaccff4760b992cd61db3da7a66fe964d08f782
AmauriLima <amauri.plimaj@gmail.com>
2022-03-04 09:01:45 -0300

    feat: configurando menus

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -24,13 +24,15 @@ import qualified Data.ByteString.Char8 as B
 main :: IO()
 main = do
     putStrLn "Boas vindas!"
-    putStrLn "Selecione uma das opções abaixo:\n"
     showMenu
 
 showMenu:: IO()
 showMenu = do
+    putStrLn "\nSelecione uma das opções abaixo:\n"
+
     putStrLn "1 - Sou Administrador"
     putStrLn "2 - Sou Cliente"
+    putStrLn "3 - Sair"
 
     opcao <- getLine
     menus opcao
@@ -39,7 +41,11 @@ menus :: String -> IO()
 menus x
     | x == "1" = menuAdm
     | x == "2" = menuCliente
+    | x == "3" = encerrarSessao
     | otherwise = invalidOption showMenu
+
+encerrarSessao:: IO()
+encerrarSessao = putStrLn "Saindo... Até a próxima!" 
 
 invalidOption :: IO() -> IO()
 invalidOption f = do
@@ -54,6 +60,7 @@ menuAdm = do
     putStrLn "3 - Alterar disponibilidade hotelzinho"
     putStrLn "4 - listar resumo de atendimentos"
     putStrLn "5 - Atualizar contato Adm"
+    putStrLn "6 - Voltar"
 
     opcao <- getLine
     opcaoAdm opcao
@@ -65,6 +72,7 @@ opcaoAdm x
     | x == "3" = alterarDisponibilidadeHotelzinho
     | x == "4" = listarResumoDeAtendimentos
     | x == "5" = atualizarContatoAdm
+    | x == "6" = showMenu
     | otherwise = invalidOption menuAdm
 
 listarResumoDeAtendimentos :: IO()
@@ -133,6 +141,7 @@ menuCliente = do
     putStrLn "\nSelecione uma das opções abaixo:"
     putStrLn "1 - Se cadastrar como cliente"
     putStrLn "2 - Logar no sistema como cliente"
+    putStrLn "3 - Voltar ao menu principal"
 
     opcao <- getLine
     opcaoCliente opcao
@@ -141,6 +150,7 @@ opcaoCliente:: String -> IO()
 opcaoCliente x
     | x == "1" = cadastrarComoCliente
     | x == "2" = logarComoCliente
+    | x == "3" = showMenu
     | otherwise = invalidOption menuCliente
 
 segundoMenuCliente :: IO()
@@ -263,5 +273,9 @@ logarComoCliente = do
                 menuCliente
             hClose file
     else do
-        putStrLn "Cliente não cadastrado. Por favor, cadastre-se"
-        cadastrarComoCliente
+        putStrLn "Cliente não cadastrado."
+        putStrLn "Deseja fazer o cadastro agora? (s/n):"
+        op <- getLine;
+        if op == "s" then do 
+            cadastrarComoCliente
+        else menuCliente
