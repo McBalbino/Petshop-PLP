f820c9bc8eda67c2f82de5332b53b8c2999e9b7e
Carlos Pamplona <carlos.pamplona.filho@ccc.ufcg.edu.br>
2022-04-01 08:01:47 -0300

    limpando o menu antes de chamá-lo de volta

--- Prolog/menu.pl
+++ Prolog/menu.pl
@@ -57,8 +57,8 @@ menuCliente :-
 	writeln("3 - Ver contato do administrador"),
 	writeln("0 - Retornar ao menu principal"),
 	read_line_to_string(user_input, Option),
-	(Option == "1" -> tty_clear, cadastraCliente, menuCliente;
-	Option == "2" -> (tty_clear, login_cliente(Email) -> tty_clear, segundoMenuCliente(Email) ; mostraMenu);
+	(Option == "1" -> tty_clear, cadastraCliente,tty_clear, menuCliente;
+	Option == "2" -> (tty_clear, login_cliente(Email) -> tty_clear, segundoMenuCliente(Email) ; tty_clear, mostraMenu);
 	Option == "3" -> (tty_clear, exibir_contato_admin, tty_clear, menuCliente);
 	Option == "0" -> tty_clear, mostraMenu;
 	opcaoInvalida,
